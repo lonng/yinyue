34b606b68ac1d09919d7f78f4903e3bfcdc6fa9e
Chris Lonng <chris@lonng.org>
2018-05-30 23:31:24 +0800

    define adapters

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,3 +1,26 @@
-pub trait Adapter {
+pub struct Song {}
 
+pub trait Adapter {
+    fn song_list() -> Result<Vec<Song>, Error>;
+}
+
+pub struct Error {}
+
+pub fn parse_adapter(rawurl: String) -> Result<Adapter, Error> {}
+
+struct SongAdapter {
+    id: String
+}
+
+struct PlaylistAdapter {
+    id: String
+}
+
+struct AlbumAdapter {
+    id: String
+}
+
+struct CommonAdapter {
+    id: String,
+    url: String,
 }
