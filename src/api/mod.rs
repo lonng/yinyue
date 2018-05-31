616fd419af9fb6fa66db2ff839b4631e443ca0ec
Chris Lonng <chris@lonng.org>
2018-05-31 08:19:13 +0800

    parse adapter

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,15 +1,26 @@
+extern crate url;
+
 pub struct Song {}
 
 pub trait Adapter {
-    fn song_list() -> Result<Vec<Song>, Error>;
+    fn song_list(&self) -> Result<Vec<Song>, Error>;
 }
 
-pub struct Error {}
+pub enum Error {}
 
-pub fn parse_adapter(rawurl: String) -> Result<Adapter, Error> {}
+pub fn parse_adapter(rawurl: String) -> Result<Box<Adapter>, Error> {
+    //let result = url::Url::parse(rawurl.as_str())?;
+    Ok(Box::new(SongAdapter{id: "1000".to_string()}))
+}
 
 struct SongAdapter {
     id: String
+}
+
+impl Adapter for SongAdapter {
+    fn song_list(&self) -> Result<Vec<Song>, Error> {
+        unimplemented!()
+    }
 }
 
 struct PlaylistAdapter {
